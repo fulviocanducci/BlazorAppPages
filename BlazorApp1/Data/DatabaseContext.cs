848cc158cf4077cb84cf1e86d214c6ec52b3efb9
Fulvio Cezar Canducci Dias <fulviocanducci@hotmail.com>
2023-03-24 16:03:33 -0300

    Add project files.

---
+++ BlazorApp1/Data/DatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorApp1.Data
+{
+   public class DatabaseContext : DbContext
+   {
+      public DatabaseContext(DbContextOptions<DatabaseContext> options)
+         : base(options) { }
+
+      public DbSet<Car> Car { get; set; }
+   }
+
+   [Table("cars")]
+   public class Car
+   {
+      [Key()]
+      public long Id { get; set; }
+
+      [Required(ErrorMessage = "Digite o nome do carro.")]
+      [MaxLength(100, ErrorMessage = "Máximo 100 caracteres.")]
+      public string Name { get; set; } = string.Empty;
+   }
+}
---
+++ BlazorApp1/Program.cs
@@ -0,0 +1,34 @@
+using BlazorApp1.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp1
+{
+   public class Program
+   {
+      public static void Main(string[] args)
+      {
+         var builder = WebApplication.CreateBuilder(args);         
+         builder.Services.AddSingleton<WeatherForecastService>();
+         builder.Services.AddDbContext<DatabaseContext>(configuration =>
+         {
+            configuration.UseMySql("Server=127.0.0.1;Database=db;Uid=root;Pwd=senha;", new MySqlServerVersion(new Version(8, 0, 31)));
+         });
+         builder.Services.AddRazorPages();
+         builder.Services.AddServerSideBlazor();
+         var app = builder.Build();
+         if (!app.Environment.IsDevelopment())
+         {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+         }
+         app.UseHttpsRedirection();
+         app.UseStaticFiles();
+         app.UseRouting();
+         app.MapBlazorHub();
+         app.MapFallbackToPage("/_Host");
+         app.Run();
+      }
+   }
+}
